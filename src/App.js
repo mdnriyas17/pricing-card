704b18948e0e5555398fae936f75bd81343d7dfd
mdnriyas17 <mdnriyas@gmail.com>
2023-04-05 15:48:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React,{useState} from 'react';
 import Pricing from './pages/pricing';
+import Counter from './pages/counter';
 function App() {
- 
+ const [page]=useState("Pricing")
   return (
     <div className="App">
-<Pricing/> 
+{page==='Pricing'&& <Pricing/>} 
+{page==='counder'&& <Counter/>} 
     </div>
   );
 }
